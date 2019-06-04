fdaec298261f463efcb62806b9f8b9d6d483a166
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-06-04 14:16:23 +0300

    fixed

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 function daysInYear(year) {
-    try {
-        if (Number.isInteger(year) === false) {
-            throw  'exception';
+
+        if (!Number.isInteger(year)) {
+            throw  ('exception');
         }
         return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0) ? 366 : 365;
-    } catch (e) {
-        console.log(e);
-    }
 }
 
-console.log(daysInYear( 2019));
+console.log(daysInYear( '2019'));
